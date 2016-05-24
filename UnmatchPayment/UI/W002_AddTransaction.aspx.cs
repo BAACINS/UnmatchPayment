26f79cfe5b0f8cfeaf86de5b8d8339886ec5273c
Wasan <wasan.lu@baac.or.th>
2016-05-24 17:30:09 +0700

    add table UnmatchCause

--- UnmatchPayment/UI/W002_AddTransaction.aspx.cs
+++ UnmatchPayment/UI/W002_AddTransaction.aspx.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UnmatchPayment.Database;
 
 namespace UnmatchPayment.UI
 {
     public partial class AddTransaction : System.Web.UI.Page
     {
+        dbAccountDataContext dbAcc = new dbAccountDataContext();
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetUnmatchCause()
         {
 
         }
