3697a7146370456cb9827718e31a20807503c0a4
sirapat27 <sirapat.ne@baac.or.th>
2016-07-11 11:06:22 +0700

    CancleApprove

---
+++ UnmatchPayment/UI/W009_CancleApprove.aspx
@@ -0,0 +1,145 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="W009_CancleApprove.aspx.cs" Inherits="UnmatchPayment.UI.W009_CancleApprove" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="icheck.min.js"></script>
+    <link href="../skins/polaris/polaris.css" rel="stylesheet">
+    <script type="text/javascript" language="javascript">
+        function CheckAllEmp() {
+            alert('aa');
+            <%--var GridVwHeaderChckbox = document.getElementById("<%=gvUnmatchList.ClientID %>");
+            for (i = 1; i < GridVwHeaderChckbox.rows.length; i++) {
+                GridVwHeaderChckbox.rows[i].cells[8].getElementsByTagName("INPUT")[0].checked = true;//Checkbox.checked;
+            }--%>
+        }
+        window.onload = function () {
+            var listCause = new Array();
+            var hdlistCause = document.getElementById('<%=hdListCause.ClientID%>').value;
+            var hdMaxCause = document.getElementById('<%=hdMaxCause.ClientID%>').value;
+
+            if (hdlistCause != '') {
+                listCause = hdlistCause.split(',');
+                for (var j = 0; j < listCause.length; j++) {
+                    if (listCause[j] != '') {
+                        document.getElementById("radio" + listCause[j]).checked = true;
+                    }
+                }
+            }
+        };
+
+        function rdbChecked(CauseID) {
+            var listCause = new Array();
+            var hdlistCause = document.getElementById('<%=hdListCause.ClientID%>').value;
+            listCause = hdlistCause.split(',');
+            if (document.getElementById("radio" + CauseID).checked) {
+                listCause.push(CauseID);
+            }
+            else {
+                for (var i = listCause.length; i--;) {
+                    if (listCause[i] == CauseID) {
+                        listCause.splice(i, 1);
+                    }
+                }
+            }
+
+            document.getElementById('<%=hdListCause.ClientID%>').value = listCause;
+        }
+
+        //$(document).ready(function () {
+        //    $('input').iCheck({
+        //        checkboxClass: 'icheckbox_polaris',
+        //        radioClass: 'iradio_polaris',
+        //        increaseArea: '-10%' // optional
+        //    });
+        //});
+    </script>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h1>ยกเลิกอนุมัติรายการ Unmatch</h1>
+    <br />
+
+    <asp:CheckBoxList ID="cbCauseList" runat="server">
+    </asp:CheckBoxList>
+    <div>
+        <table width="100%">
+            <tr>
+                <td>
+                    <asp:Literal ID="ltrbl" runat="server"></asp:Literal>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <br />
+                    <asp:Button ID="btnSearch" runat="server" Text="ค้นหา" CssClass="button" OnClick="btnSearch_Click" />
+                </td>
+            </tr>
+        </table>
+        <asp:HiddenField ID="hdListCause" runat="server" />
+        <asp:HiddenField ID="hdMaxCause" runat="server" />
+    </div>
+    <br />
+
+    <p>
+        <asp:Label ID="lblDataNotFound" runat="server" Text="ไม่พบข้อมูล" ForeColor="Red"></asp:Label>
+        <asp:GridView ID="gvUnmatchList" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" AutoGenerateColumns="False">
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:BoundField DataField="TellerPaymentDetailID" HeaderText="รหัสอ้างอิง" />
+                <asp:BoundField DataField="CompCode" HeaderText="รหัสรายการ" />
+                <asp:BoundField DataField="Amount" HeaderText="จำนวนเงิน" />
+                <asp:BoundField DataField="Ref1" HeaderText="Ref1" />
+                <asp:BoundField DataField="Ref2" HeaderText="Ref2" />
+                <asp:BoundField DataField="RefName" HeaderText="Ref อ้างอิงชื่อ" />
+                <asp:BoundField DataField="PaymentDate" HeaderText="วันที่ชำระ" />
+                <asp:BoundField DataField="CauseDescription" HeaderText="รายการ Unmatched" />
+                <asp:BoundField HeaderText="คืนเงินด้วย SPIN" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:RadioButton ID="rdbSPIN" Value="SPIN" runat="server" GroupName="rdbReturnType" Enabled='<%# Eval("IsSpin") %>' Checked='<%# CheckReturnDefault(DataBinder.Eval(Container.DataItem,"IsSpin"),DataBinder.Eval(Container.DataItem,"IsGL")) %>' />
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                </asp:TemplateField>
+                <asp:BoundField HeaderText="คืนเงินด้วย GL" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:RadioButton ID="rdbGL" Value="GL" runat="server" GroupName="rdbReturnType" Enabled='<%# Eval("IsGL") %>' Checked='<%# CheckReturnDefault(DataBinder.Eval(Container.DataItem,"IsGL"),DataBinder.Eval(Container.DataItem,"IsSpin")) %>' />
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                </asp:TemplateField>
+                <asp:BoundField HeaderText="" />
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:Button ID="btnView" runat="server" Text=' แก้ไข '
+                            CssClass="button" OnClick="btnView_Click" CommandArgument='<%# Eval("TellerPaymentDetailID") %>' />
+                    </ItemTemplate>
+                    <ItemStyle HorizontalAlign="Center"></ItemStyle>
+                </asp:TemplateField>
+                <asp:BoundField HeaderText="ยกเลิกอนุมัติ" />
+                <asp:TemplateField>
+                    <HeaderTemplate>
+                        <asp:CheckBox ID="chkboxSelectAll" AutoPostBack="true" runat="server" OnCheckedChanged="chkboxSelectAll_CheckedChanged" />
+                    </HeaderTemplate>
+                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chkApp" runat="server" CommandArgument='<%#Eval("TellerPaymentDetailID")%>'></asp:CheckBox>
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            <EditRowStyle BackColor="#2461BF" />
+            <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#F5F7FB" />
+            <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+            <SortedDescendingCellStyle BackColor="#E9EBEF" />
+            <SortedDescendingHeaderStyle BackColor="#4870BE" />
+        </asp:GridView>
+    </p>
+    <asp:Button ID="btnApprove" runat="server" Text="อนุมัติ" CssClass="button" OnClick="btnApprove_Click" />
+    <p>
+    </p>
+
+</asp:Content>
---
+++ UnmatchPayment/UI/W009_CancleApprove.aspx.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UnmatchPayment.Class;
+using UnmatchPayment.Database;
+
+namespace UnmatchPayment.UI
+{
+    public partial class W009_CancleApprove : System.Web.UI.Page
+    {
+        #region Property
+        dbAccountDataContext dbAcc = new dbAccountDataContext();
+        C001_DataMng DataMNG = new C001_DataMng();
+        private EMPLOYEE_SELECTResult Emp
+        {
+            get
+            {
+                if (Session["Emp"] == null)
+                {
+                    Session["Emp"] = null;
+                }
+                return (EMPLOYEE_SELECTResult)Session["Emp"];
+            }
+            set
+            {
+                Session["Emp"] = value;
+            }
+        }
+        private DataTable dtUnmatchedCause
+        {
+            get
+            {
+                DataTable dt = new DataTable();
+                if ((DataTable)ViewState["dtUnmatchedCause"] != null)
+                    dt = (DataTable)ViewState["dtUnmatchedCause"];
+                return dt;
+            }
+            set
+            {
+                ViewState["dtUnmatchedCause"] = value;
+            }
+        }
+
+        #endregion
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetUnmatchCause();
+                GetdataClaim();
+            }
+        }
+        public bool CheckReturnDefault(object item1, object item2)
+        {
+
+            if (item1.ToString().ToLower() == "true" && item2.ToString().ToLower() == "false")
+                return true;
+            else
+                return false;
+        }
+
+        private void GetdataClaim()
+        {
+            var statusList = new string[] { "02" };
+            List<string> CauseList = hdListCause.Value.ToString().Split(',').ToList<string>();
+            //var CauseList = new int[] { 1,2 };
+            DataTable dtUnmatch = new DataTable();
+            var dtAcc = (from claim in dbAcc.tbUnmatchPayments
+                         join cause in dbAcc.UnmatchCauses on claim.CauseID equals cause.CauseID
+                         where statusList.Contains(claim.StatusCode)
+                         && CauseList.Contains(claim.CauseID.ToString())
+                         && claim.ModifiedBy != Emp.USER_ID
+                         select new
+                         {
+                             claim.TellerPaymentDetailID,
+                             claim.CompCode,
+                             claim.Amount,
+                             claim.Ref1,
+                             claim.Ref2,
+                             claim.RefName,
+                             claim.PaymentDate,
+                             cause.CauseID,
+                             cause.CauseDescription,
+                             cause.isSpin,
+                             cause.isGL
+                         }
+                         ).OrderBy(x => x.CauseID);
+
+            dtUnmatch = DataMNG.LINQToDataTable(dtAcc);
+
+            gvUnmatchList.DataSource = dtUnmatch;
+            gvUnmatchList.DataBind();
+            if (dtUnmatch.Rows.Count > 0)
+            {
+                btnApprove.Visible = true;
+                lblDataNotFound.Visible = false;
+            }
+            else
+            {
+                btnApprove.Visible = false;
+                lblDataNotFound.Visible = true;
+            }
+        }
+
+        private void GetUnmatchCause()
+        {
+            //DataTable dtUnmatch = new DataTable();
+            var dtAcc = from Cause in dbAcc.UnmatchCauses
+                        where Cause.isActive == true
+                        select new
+                        {
+                            Cause.CauseID,
+                            Cause.CauseDescription,
+                            isUpdateUnmatched = Convert.ToInt16(Cause.isUpdateUnmatched),
+                            isCompCode = Convert.ToInt16(Cause.isCompCode),
+                            isAmount = Convert.ToInt16(Cause.isAmount),
+                            isRef1 = Convert.ToInt16(Cause.isRef1),
+                            isRef2 = Convert.ToInt16(Cause.isRef2),
+                            isRefName = Convert.ToInt16(Cause.isRefName),
+                            isPaymentdate = Convert.ToInt16(Cause.isPaymentdate),
+                            isRefund = Convert.ToInt16(Cause.isRefund),
+                            isUplaodFile = Convert.ToInt16(Cause.isUploadFile)
+                        };
+
+            dtUnmatchedCause = DataMNG.LINQToDataTable(dtAcc);
+
+            StringBuilder strCause = new StringBuilder();
+            List<string> listCause = new List<string>();
+            hdMaxCause.Value = dtUnmatchedCause.Rows.Count.ToString();
+            for (int i = 0; i < dtUnmatchedCause.Rows.Count; i++)
+            {
+
+
+                string strCauseID = dtUnmatchedCause.Rows[i]["CauseID"].ToString();
+                string strCauseDes = dtUnmatchedCause.Rows[i]["CauseDescription"].ToString();
+
+                listCause.Add(strCauseID);
+                if (i % 2 == 0)
+                {
+                    //add radio in literal
+                    strCause.Append(string.Format(@"
+                    <div>
+                    <span class='spanleft'>
+                    <input type = 'checkbox' name='radio{0}' id='radio{0}' class='radio' runat='server' onclick='rdbChecked({0})' />
+                    <label for= 'radio{0}' > {1} </label>
+                    </span>", strCauseID, strCauseDes));
+                }
+                else
+                {
+                    strCause.Append(string.Format(@"
+                    <span class='spanright'>
+                    <input type = 'checkbox' name='radio{0}' id='radio{0}' class='radio' runat='server' onclick='rdbChecked({0})' />
+                    <label for= 'radio{0}' > {1} </label>
+                    </span>
+                    </div>", strCauseID, strCauseDes));
+                }
+            }
+            hdListCause.Value = listCause.Aggregate((x, y) => x + "," + y);
+            ltrbl.Text = strCause.ToString();
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = string.Empty;
+                string _strTellerID = ((Button)sender).CommandArgument;
+                Application["TellerID"] = _strTellerID;
+                Application["isEdit"] = "01";
+
+                Response.Redirect("W002_AddTransaction.aspx");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void btnApprove_Click(object sender, EventArgs e)
+        {
+            foreach (GridViewRow row in gvUnmatchList.Rows)
+            {
+                RadioButton rdbSPIN = (RadioButton)row.FindControl("rdbSPIN");
+                RadioButton rdbGL = (RadioButton)row.FindControl("rdbGL");
+                CheckBox cbApproved = (CheckBox)row.FindControl("chkApp");
+                if (cbApproved != null)
+                {
+                    if (cbApproved.Checked.ToString().ToLower() == "true" && !(rdbGL.Enabled == true && rdbSPIN.Enabled == true && rdbGL.Checked == false && rdbSPIN.Checked == false))
+                    {
+                        string _strTellerID = row.Cells[0].Text;
+
+                        if (!string.IsNullOrEmpty(_strTellerID))
+                        {
+                            tbUnmatchPayment UP = dbAcc.tbUnmatchPayments.Single(unMatched => unMatched.TellerPaymentDetailID == Convert.ToInt32(_strTellerID));
+
+                            UP.StatusCode = "02";
+                            UP.ApproveBy = Emp.USER_ID;
+                            UP.ApproveDate = DateTime.Now;
+                            UP.ModifiedBy = Emp.USER_ID;
+                            UP.ModifiedDate = DateTime.Now;
+                            //check Return Type
+                            if (rdbSPIN.Checked)
+                                UP.ReturnTypeID = 2;
+                            else if (rdbGL.Checked)
+                                UP.ReturnTypeID = 3;
+                            else
+                                UP.ReturnTypeID = 1;
+
+                            dbAcc.SubmitChanges();
+
+                        }
+
+                    }
+                }
+            }
+            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "js", "alert('อนุมัติเสร็จสิ้น');", true);
+            GetdataClaim();
+        }
+
+        protected void chkboxSelectAll_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox ChkBoxHeader = (CheckBox)gvUnmatchList.HeaderRow.FindControl("chkboxSelectAll");
+            bool a = ((CheckBox)sender).Checked;
+            foreach (GridViewRow row in gvUnmatchList.Rows)
+            {
+                CheckBox ChkBoxRows = (CheckBox)row.FindControl("chkApp");
+                if (ChkBoxHeader.Checked == true)
+                {
+                    ChkBoxRows.Checked = true;
+                }
+                else
+                {
+                    ChkBoxRows.Checked = false;
+                }
+            }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            GetdataClaim();
+
+        }
+    }
+}
