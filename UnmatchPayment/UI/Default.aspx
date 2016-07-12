a0c1a2aba93f0fbbaa436fa7919f9f6d79a0db2e
sirapat27 <sirapat.ne@baac.or.th>
2016-07-12 13:50:37 +0700

    Manual Unmatchpayment

--- UnmatchPayment/UI/Default.aspx
+++ UnmatchPayment/UI/Default.aspx
@@ -2,4 +2,8 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <br /><br /><br /><br />
+    <p style="text-align:center">
+        <asp:LinkButton ID="lnk" runat="server" OnClick="lnk_Click">คู่มือการใช้งาน</asp:LinkButton></p>
+    <p>&nbsp;</p>
 </asp:Content>
--- UnmatchPayment/UI/Default.aspx.cs
+++ UnmatchPayment/UI/Default.aspx.cs
@@ -4,14 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UnmatchPayment.Database;
 
 namespace UnmatchPayment.UI
 {
     public partial class Default : System.Web.UI.Page
     {
+        private EMPLOYEE_SELECTResult Emp
+        {
+            get
+            {
+                if (Session["Emp"] == null)
+                {
+                    Session["Emp"] = null;
+                }
+                return (EMPLOYEE_SELECTResult)Session["Emp"];
+            }
+            set
+            {
+                Session["Emp"] = value;
+            }
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+        }
+
+        protected void lnk_Click(object sender, EventArgs e)
+        {
+            string strFileName = "UnmatchManual.pdf";
+            string strSourceFile = string.Empty;
+            if (Emp.isBranch == 0)
+                strSourceFile = Server.MapPath("~/UserManual/ManualforHQ.pdf");
+            else
+                strSourceFile = Server.MapPath("~/UserManual/ManualforBranch.pdf");
+            System.Web.HttpResponse response = System.Web.HttpContext.Current.Response;
+            response.ClearContent();
+            response.Clear();
+            response.ContentType = "text/plain";
+            response.AddHeader("Content-Disposition",
+                               "attachment; filename=" + strFileName + ";");
+            response.TransmitFile(strSourceFile);
+            response.Flush();
+            response.End();
         }
     }
 }
